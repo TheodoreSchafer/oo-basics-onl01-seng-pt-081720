ef8aab0b9ee7a2780c0b55bfccb9dd69c1cb17df
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-02 16:16:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,7 @@
 class Book
+
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
@@ -11,25 +14,9 @@ class Book
     @author = author
   end
 
-  def author
-    @author
-  end
 
-  def page_count=(num)
-    @page_count = num
-  end
 
-  def page_count
-    @page_count
-  end
 
-  def genre= (genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
