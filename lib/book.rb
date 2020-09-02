4a7f81cb569c3298608e785555c032b189ccae4a
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-02 16:13:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -13,7 +13,27 @@ class Book
 
   def author
     @author
-  end 
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre= (genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+    puts "Flips the page...wow, you read fast!"
+  end
 
 
 end
